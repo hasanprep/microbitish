16029acfcb5078b5ab458564a5971a79401fb0ef
Simon <simon@hasanprep.org>
2023-12-13 20:33:20 -0700

    Added more comments

--- code/display/microbit/scroll_text_delay_microbit.py
+++ code/display/microbit/scroll_text_delay_microbit.py
@@ -1,16 +1,16 @@
 from microbit import *
 
 while True:
-    display.scroll('Python', delay=100)  # Scroll text with 100ms delay``
-    sleep(1000)
-    display.scroll('Python', delay=50)   # Scroll text with 50ms delay
-    sleep(1000)
-    display.scroll('Python', delay=25)   # Scroll text with 25ms delay
-    sleep(1000)
-    display.scroll('Python', delay=10)   # Scroll text with 10ms delay
-    sleep(1000)
-    display.scroll('Python', delay=1)    # Scroll text with 1ms delayq
-    sleep(1000)
-    display.scroll('Python', delay=0)    # Scroll text with 0ms delay
-    sleep(1000)
+    display.scroll('Python', delay=100) # Scroll text with 100ms delay``
+    sleep(1000)                         # Wait 1 second
+    display.scroll('Python', delay=50)  # Scroll text with 50ms delay
+    sleep(1000)                         # Wait 1 second
+    display.scroll('Python', delay=25)  # Scroll text with 25ms delay
+    sleep(1000)                         # Wait 1 second
+    display.scroll('Python', delay=10)  # Scroll text with 10ms delay
+    sleep(1000)                         # Wait 1 second
+    display.scroll('Python', delay=1)   # Scroll text with 1ms delayq
+    sleep(1000)                         # Wait 1 second 
+    display.scroll('Python', delay=0)   # Scroll text with 0ms delay
+    sleep(1000)                         # Wait 1 second
     
--- code/display/microbit/scroll_text_microbit.py
+++ code/display/microbit/scroll_text_microbit.py
@@ -1,6 +1,6 @@
 from microbit import *
 
 while True:
-    display.scroll('Python')             # Scroll text
-    sleep(1000)
+    display.scroll('Python') # Scroll text
+    sleep(1000)              # Wait 1 second
                    
